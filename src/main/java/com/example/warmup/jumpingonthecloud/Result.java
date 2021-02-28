3468f2eb0c3fd1dd6deeab2b574a424cc7771a49
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-02-28 17:24:40 -0500

    Update solution

--- src/main/java/com/example/warmup/jumpingonthecloud/Result.java
+++ src/main/java/com/example/warmup/jumpingonthecloud/Result.java
@@ -3,7 +3,29 @@ package com.example.warmup.jumpingonthecloud;
 public class Result {
 
     static int jumpingOnClouds(int[] c) {
+        int jumps = 0;
+        int i = 0;
+        if (c.length == 2 || c.length == 3) {
+            return 1;
+        }
 
-        return 0;
+        while (i < c.length - 1) {
+            if (canJumpTwo(c, i)) {
+                i += 2;
+            } else {
+                i += 1;
+            }
+            jumps++;
+        }
+
+        return jumps;
+    }
+
+    private static boolean canJumpTwo(int[] c, int i) {
+        return atNextToLastCloud(c, i) && c[i + 2] == 0;
+    }
+
+    private static boolean atNextToLastCloud(int[] c, int i) {
+        return i < c.length - 2;
     }
 }
--- src/test/java/com/example/warmup/jumpingonthecloud/ResultTest.java
+++ src/test/java/com/example/warmup/jumpingonthecloud/ResultTest.java
@@ -11,4 +11,28 @@ class ResultTest {
         var clouds = new int[]{0, 0, 1, 0, 0, 1, 0};
         assertThat(Result.jumpingOnClouds(clouds)).isEqualTo(4);
     }
+
+    @Test
+    void shouldReturnTheCorrectNumberOfJumps2() {
+        var clouds = new int[]{0, 1, 0, 0, 1, 0, 0, 1, 0};
+        assertThat(Result.jumpingOnClouds(clouds)).isEqualTo(5);
+    }
+
+    @Test
+    void shouldReturnTheCorrectNumberOfJumps3() {
+        var clouds = new int[]{0, 0, 0, 1, 0, 0};
+        assertThat(Result.jumpingOnClouds(clouds)).isEqualTo(3);
+    }
+
+    @Test
+    void shouldReturnTheCorrectNumberOfJumpsForMinimumArray() {
+        var clouds = new int[]{0, 0};
+        assertThat(Result.jumpingOnClouds(clouds)).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReturnTheLeastAmountOfJumpsWhenOnlyTwoPossibilities() {
+        var clouds = new int[]{0, 0, 0};
+        assertThat(Result.jumpingOnClouds(clouds)).isEqualTo(1);
+    }
 }
