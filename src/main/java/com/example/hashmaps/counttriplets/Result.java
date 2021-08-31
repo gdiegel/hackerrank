20475f6e58881bb8909553e0781a6d9bcc91012a
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-08-31 11:48:31 -0500

    Submit correct but bad performance solution

--- src/main/java/com/example/hashmaps/counttriplets/Result.java
+++ src/main/java/com/example/hashmaps/counttriplets/Result.java
@@ -4,8 +4,27 @@ import java.util.List;
 
 public class Result {
 
-    static long countTriplets(List<Long> arr, long r) {
+    /* 1, 2, 2, 4
+       0, 1, 2, 3
 
-        return 0L;
+       (0,1), (1,2), (2,2), (3,4)
+    */
+    static long countTriplets(List<Long> arr, long r) {
+        long numberOfTriplets = 0L;
+
+        for (int i = 0; i < arr.size() - 2; i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                for (int k = j + 1; k < arr.size(); k++) {
+                    final long entryI = arr.get(i);
+                    final long entryJ = arr.get(j);
+                    final long entryK = arr.get(k);
+                    if (entryJ == entryI * r && entryK == entryJ * r) {
+                        numberOfTriplets++;
+                    }
+                }
+            }
+        }
+
+        return numberOfTriplets;
     }
 }
--- src/test/java/com/example/hashmaps/counttriplets/ResultTest.java
+++ src/test/java/com/example/hashmaps/counttriplets/ResultTest.java
@@ -2,9 +2,19 @@ package com.example.hashmaps.counttriplets;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ResultTest {
 
     @Test
-    void countTriplets() {
+    void shouldReturn2ForExampleOne() {
+        assertThat(Result.countTriplets(List.of(1L, 2L, 2L, 4L), 2)).isEqualTo(2);
+    }
+
+    @Test
+    void shouldReturn6ForExampleTwo() {
+        assertThat(Result.countTriplets(List.of(1L, 3L, 9L, 9L, 27L, 81L), 3)).isEqualTo(6);
     }
 }
