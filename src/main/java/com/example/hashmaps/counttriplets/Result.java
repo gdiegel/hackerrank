0fa43041ae5c67ea2545485667e738d3345848e6
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-08-31 10:22:26 -0500

    Add skeleton

---
+++ src/main/java/com/example/hashmaps/counttriplets/Result.java
@@ -0,0 +1,11 @@
+package com.example.hashmaps.counttriplets;
+
+import java.util.List;
+
+public class Result {
+
+    static long countTriplets(List<Long> arr, long r) {
+
+        return 0L;
+    }
+}
---
+++ src/test/java/com/example/hashmaps/counttriplets/ResultTest.java
@@ -0,0 +1,10 @@
+package com.example.hashmaps.counttriplets;
+
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    void countTriplets() {
+    }
+}
