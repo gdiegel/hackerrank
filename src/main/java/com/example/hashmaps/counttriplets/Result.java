414bbf801039a3c1e0e68b298f648ee88dd09c45
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-09-12 18:16:56 -0500

    Optimize using HashMap

--- src/main/java/com/example/hashmaps/counttriplets/Result.java
+++ src/main/java/com/example/hashmaps/counttriplets/Result.java
@@ -1,30 +1,138 @@
 package com.example.hashmaps.counttriplets;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Result {
 
-    /* 1, 2, 2, 4
-       0, 1, 2, 3
+    /*
+    [1,1,1,1,1]
+     0,1,2,3,4
+    Checking [1,1,1] at (0,1,2): TRUE
+    Checking [1,1,1] at (0,1,3): TRUE
+    Checking [1,1,1] at (0,2,3): TRUE
+    Checking [1,1,1] at (1,2,3): TRUE
 
-       (0,1), (1,2), (2,2), (3,4)
+    1=0,1,2,3,4
+    0,1,2, 0,1,3, 0,1,4, 0,2,3, 0,2,4, 0,3,4, 1,2,3, 1,2,4, 1,3,4, 2,3,4
+
+    1 (1,1)
+
+    [1,2,2,4]
+     0,1,2,3
+    Checking [1,2,2] at (0,1,2): FALSE
+    Checking [1,2,4] at (0,1,3): TRUE
+    Checking [1,2,4] at (0,2,3): TRUE
+    Checking [2,2,4] at (1,2,3): FALSE
+
+    1=0
+    2=1,2
+    4=3
+    1 (2,4): 1*2*1=2 (for values 0, 1,2 and 3 find combinatory triplets where i < j < k)
+
+    [1,3,9,3,1]
+     0,1,2,3,4
+    Checking [1,3,9] at (0,1,2): TRUE
+    Checking [1,3,3] at (0,1,3): FALSE
+    Checking [1,3,1] at (0,1,4): FALSE
+    Checking [1,9,3] at (0,2,3): FALSE
+    Checking [1,9,1] at (0,2,4): FALSE
+    Checking [1,3,1] at (0,3,4): FALSE
+    Checking [3,9,3] at (1,2,3): FALSE
+    Checking [3,9,1] at (1,2,4): FALSE
+    Checking [3,3,1] at (1,3,4): FALSE
+    Checking [9,3,1] at (2,3,4): FALSE
+
+    1=0,4
+    3=1,3
+    9=2
+
+    1 (3,9): 0,1,2 (i < j < k) (for values i=0,4, j=1,3 and k=2 find combinatory triples where i<j<k)
+
+    [1,3,9,3,9,2]
+     0,1,2,3,4,5
+
+     1=0
+     2=5
+     3=1,3
+     9=2,4
+     1 (3,9): 0, 1,3, 2,4 (for values i=0, j=1,3 and k=2,4 find combinatory triplets where i<j<k):
+     0,1,2
+     0,1,4
+     0,3,4
+
     */
     static long countTriplets(List<Long> arr, long r) {
         long numberOfTriplets = 0L;
+        ConcurrentMap<Long, List<Integer>> valueToIndices = calculateValueToIndices(arr);
+        final Set<Long> keys = valueToIndices.keySet();
+        if (keys.size() == 1 && r == 1L) {
+            final int n = valueToIndices.values().stream().toList().get(0).size();
+            final int k = 3;
+            return factorial(n).divide(factorial(n - k).multiply(BigInteger.valueOf(6))).longValue();
 
-        for (int i = 0; i < arr.size() - 2; i++) {
-            for (int j = i + 1; j < arr.size(); j++) {
-                for (int k = j + 1; k < arr.size(); k++) {
-                    final long entryI = arr.get(i);
-                    final long entryJ = arr.get(j);
-                    final long entryK = arr.get(k);
-                    if (entryJ == entryI * r && entryK == entryJ * r) {
+        }
+        System.out.printf("Keys: %s%n", keys);
+        for (final Long i : keys) {
+            long j = i * r;
+            long k = j * r;
+            if (keys.contains(j) && keys.contains(k)) {
+                System.out.printf("i,j,k: [%d,%d,%d]%n", i, j, k);
+                final List<Integer> listI = valueToIndices.get(i);
+                final List<Integer> listJ = valueToIndices.get(j);
+                final List<Integer> listK = valueToIndices.get(k);
+                numberOfTriplets += findNumberOfTriplets(listI, listJ, listK);
+            }
+        }
+        return numberOfTriplets;
+    }
+
+    private static ConcurrentMap<Long, List<Integer>> calculateValueToIndices(List<Long> arr) {
+        ConcurrentMap<Long, List<Integer>> valueToIndices = new ConcurrentHashMap<>();
+        for (int i = 0; i < arr.size(); i++) {
+            final Long key = arr.get(i);
+            final int index = i;
+            valueToIndices.compute(key, (k, v) -> {
+                if (v == null)
+                    v = new ArrayList<>();
+                v.add(index);
+                return v;
+            });
+        }
+        System.out.println(valueToIndices);
+        return valueToIndices;
+    }
+
+    private static long findNumberOfTriplets(List<Integer> listI, List<Integer> listJ, List<Integer> listK) {
+        long numberOfTriplets = 0L;
+        for (int i = 0; i < listI.size(); i++) {
+            for (int j = 0; j < listJ.size(); j++) {
+                for (int k = 0; k < listK.size(); k++) {
+                    final long entryI = listI.get(i);
+                    final long entryJ = listJ.get(j);
+                    final long entryK = listK.get(k);
+                    System.out.printf("Checking [%d,%d,%d] at (%d,%d,%d):", entryI, entryJ, entryK, i, j, k);
+                    if (entryI < entryJ && entryJ < entryK) {
+                        System.out.printf(" TRUE%n");
                         numberOfTriplets++;
+                    } else {
+                        System.out.printf(" FALSE%n");
                     }
                 }
             }
         }
-
         return numberOfTriplets;
+    }
+
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        System.out.printf("Factorial of %d is %d%n", n, result);
+        return result;
     }
 }
--- src/test/java/com/example/hashmaps/counttriplets/ResultTest.java
+++ src/test/java/com/example/hashmaps/counttriplets/ResultTest.java
@@ -2,8 +2,14 @@ package com.example.hashmaps.counttriplets;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ResultTest {
@@ -17,4 +23,33 @@ class ResultTest {
     void shouldReturn6ForExampleTwo() {
         assertThat(Result.countTriplets(List.of(1L, 3L, 9L, 9L, 27L, 81L), 3)).isEqualTo(6);
     }
+
+    @Test
+    void shouldReturn1ForExampleThree() {
+        assertThat(Result.countTriplets(List.of(1L, 3L, 9L, 3L, 1L), 3)).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReturn4ForExampleFour() {
+        assertThat(Result.countTriplets(List.of(1L, 1L, 1L, 1L), 1)).isEqualTo(4);
+    }
+
+    @Test
+    void should() throws IOException {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("input03.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        long ans = Result.countTriplets(arr, r);
+
+        assertThat(ans).isEqualTo(166661666700000L);
+    }
+
 }
